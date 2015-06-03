a825d02ff9dd40ab1a2901fa7f0db55f074c9d11
KevinL <kevin@littlejohn.id.au>
2015-06-03 12:26:34 +1000

    support for extra cmdline args

--- deploy.sh
+++ deploy.sh
@@ -1,7 +1,8 @@
 #!/bin/bash -e
 
 STACKNAME=$1
+shift
 PARAMS=""
 [ -f /app/params.yaml ] && PARAMS="-d=yaml:params.yaml"
 
-rainbow --update-stack-if-exists -v -r ap-southeast-2 $PARAMS --block $STACKNAME stack.json
+rainbow --update-stack-if-exists -v -r ap-southeast-2 $PARAMS --block $STACKNAME stack.json "$@"
