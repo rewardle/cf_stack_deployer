b9afc6863e3dd09cca8081d8678826a2eaa36f70
rewardle-jason <jason@rewardle.com>
2015-11-21 18:38:54 +1100

    Update deploy.sh
    removed region default so that the deployer can be used in other regions.

--- deploy.sh
+++ deploy.sh
@@ -5,4 +5,4 @@ shift
 PARAMS=""
 [ -f /app/params.yaml ] && PARAMS="-d=yaml:params.yaml"
 
-rainbow --update-stack-if-exists -v -r ap-southeast-2 $PARAMS --block $STACKNAME stack.json "$@"
+rainbow --update-stack-if-exists -v $PARAMS --block $STACKNAME stack.json "$@"
