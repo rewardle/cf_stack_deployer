8f6045a1cda4e3a98bd080b99565bec46ed273a8
Kevin Littlejohn <kevin@littlejohn.id.au>
2015-11-24 16:07:42 +1100

    better handling of failures

--- deploy.sh
+++ deploy.sh
@@ -5,8 +5,12 @@ shift
 PARAMS=""
 [ -f /app/params.yaml ] && PARAMS="-d=yaml:params.yaml"
 
-STATUS=$(aws cloudformation describe-stacks --region ${AWS_REGION:-ap-southeast-2} --stack-name $STACKNAME | jq '.Stacks[0].StackStatus')
+JSON=$(aws cloudformation describe-stacks --region ${AWS_REGION:-ap-southeast-2} --stack-name $STACKNAME)
+if [ -n "${JSON}" ]; then
+  STATUS=$(echo ${JSON} | jq '.Stacks[0].StackStatus')
+  if [ "${STATUS}" -eq "CREATE_FAILED" ]; then
+    aws cloudformation delete-stack --region ${AWS_REGION:-ap-southeast-2} --stack-name ${STACKNAME}
+  fi
+fi
 
-[ "$STATUS" -eq "CREATE_FAILED" ] && aws cloudformation delete-stack --region ${AWS_REGION:-ap-southeast-2} --stack-name $STACKNAME
-
-rainbow --update-stack-if-exists -v -r ${AWS_REGION:-ap-southeast-2} $PARAMS --block $STACKNAME stack.json "$@"
+rainbow --update-stack-if-exists -v -r ${AWS_REGION:-ap-southeast-2} ${PARAMS} --block ${STACKNAME} stack.json "$@"
