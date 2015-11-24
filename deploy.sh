734ab0047c2e161ac11959405a78e92e366858b8
Kevin Littlejohn <kevin@littlejohn.id.au>
2015-11-24 15:47:58 +1100

    remove createfailed stacks

--- deploy.sh
+++ deploy.sh
@@ -1,8 +1,12 @@
-#!/bin/bash -e
+#!/bin/bash -ex
 
 STACKNAME=$1
 shift
 PARAMS=""
 [ -f /app/params.yaml ] && PARAMS="-d=yaml:params.yaml"
+
+STATUS=$(aws cloudformation describe-stacks --region ${AWS_REGION:-ap-southeast-2} --stack-name $STACKNAME | jq '.Stacks[0].StackStatus')
+
+[ "$STATUS" -eq "CREATE_FAILED" ] && aws cloudformation delete-stack --region ${AWS_REGION:-ap-southeast-2} --stack-name $STACKNAME
 
 rainbow --update-stack-if-exists -v -r ${AWS_REGION:-ap-southeast-2} $PARAMS --block $STACKNAME stack.json "$@"
