c7656d76fc9fe42e75422ccaa3bdf5e2d75434d2
Kevin Littlejohn <kevin@littlejohn.id.au>
2015-11-24 16:17:14 +1100

    wrong equality operator

--- deploy.sh
+++ deploy.sh
@@ -8,7 +8,7 @@ PARAMS=""
 JSON=$(aws cloudformation describe-stacks --region ${AWS_REGION:-ap-southeast-2} --stack-name $STACKNAME)
 if [ -n "${JSON}" ]; then
   STATUS=$(echo ${JSON} | jq '.Stacks[0].StackStatus')
-  if [ "${STATUS}" -eq "CREATE_FAILED" ]; then
+  if [ "${STATUS}" == "CREATE_FAILED" ]; then
     aws cloudformation delete-stack --region ${AWS_REGION:-ap-southeast-2} --stack-name ${STACKNAME}
   fi
 fi
