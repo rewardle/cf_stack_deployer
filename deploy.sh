1a371fcf832f53d1d6a1c389c8c67796f1a3967f
Kevin Littlejohn <kevin@littlejohn.id.au>
2016-02-09 09:31:57 +1100

    loop until the stack is gone if deleting

--- deploy.sh
+++ deploy.sh
@@ -10,6 +10,9 @@ if [ -n "${JSON}" ]; then
   STATUS=$(echo ${JSON} | jq -r '.Stacks[0].StackStatus')
   if [ "${STATUS}" == "CREATE_FAILED" ]; then
     aws cloudformation delete-stack --region ${AWS_REGION:-ap-southeast-2} --stack-name ${STACKNAME}
+    while [ -n "$(aws cloudformation describe-stacks --region ${AWS_REGION:-ap-southeast-2} --stack-name $STACKNAME)" ]; do
+      sleep 5
+    done
   fi
 fi
 
