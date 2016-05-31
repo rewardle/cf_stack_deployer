f056bde55a8acb9c29aa1a31dcc60b5be9447673
Kevin Littlejohn <kevin@littlejohn.id.au>
2016-05-31 11:48:47 +1000

    better way to get the aws acct id

--- ci_deploy.sh
+++ ci_deploy.sh
@@ -3,18 +3,14 @@
 # Internal deployer
 
 REGION="us-west-2"
+STACKNAME=
 
 echo "--- Getting ECR credentials and logging in"
 $(docker run -it --entrypoint=aws rewardle/deployer ecr get-login --region=$REGION | tr -d '\r')
 
 echo "--- Finding the account id"
 ACCT=$(docker run -it --entrypoint=aws rewardle/deployer \
-  cloudformation describe-stack-resource \
-    --stack-name ${STACKNAME} \
-    --logical-resource-id IoTLambdaRole \
-    --region ${AWS_REGION} \
-    --query "StackResourceDetail.StackId" \
-    --output text | cut -d: -f5)
+  sts get-caller-identity --query "Account" --output text)
 
 IMGNAME="$ACCT.dkr.ecr.$REGION.amazonaws.com/rewardle/deployer:$BUILDKITE_BUILD_NUMBER"
 echo "--- Building $IMGNAME"
