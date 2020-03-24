ccad540cae799b9ed9412b6c98f97fe49b9e878c
rewardle-jason <jason@rewardle.com>
2020-03-24 11:34:47 +1100

    Catch the exception if stack does not exist

--- deploy.sh
+++ deploy.sh
@@ -9,7 +9,7 @@ shift 2
 PARAMS=""
 [ -f /app/params.yaml ] && PARAMS="-d=yaml:params.yaml"
 
-JSON=$(aws cloudformation describe-stacks --region ${AWS_REGION:-ap-southeast-2} --stack-name $STACKNAME)
+JSON=$(aws cloudformation describe-stacks --region ${AWS_REGION:-ap-southeast-2} --stack-name $STACKNAME) || ""
 if [ -n "${JSON}" ]; then
   STATUS=$(echo ${JSON} | jq -r '.Stacks[0].StackStatus')
   if [ "${STATUS}" == "CREATE_FAILED" ]; then
