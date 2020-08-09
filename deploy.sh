57749102fc5ffc92a7950b5a34fa33519cd0d04a
Jason Potter <jason@rewardle.com>
2020-08-09 18:28:22 +1000

    run rainbow with python 2

--- deploy.sh
+++ deploy.sh
@@ -24,4 +24,4 @@ if [ -n "${JSON}" ]; then
   fi
 fi
 
-rainbow --update-stack-if-exists -v -r ${AWS_REGION:-ap-southeast-2} --deployment-bucket-name ${DEPLOYMENT_BUCKET_NAME} ${PARAMS} ${STACKNAME} stack.json "$@"
+python /usr/local/bin/rainbow --update-stack-if-exists -v -r ${AWS_REGION:-ap-southeast-2} --deployment-bucket-name ${DEPLOYMENT_BUCKET_NAME} ${PARAMS} ${STACKNAME} stack.json "$@"
