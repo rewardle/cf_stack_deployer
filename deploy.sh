75c28dbfc0ab9448e7c7c4aac0c31b3b56322b64
Gaurav Mehta <gaurav.mehta@rewardle.com>
2021-07-08 00:42:56 +0530

    deployment bucket changes from master.

--- deploy.sh
+++ deploy.sh
@@ -1,11 +1,19 @@
 #!/bin/bash
 
+
 STACKNAME=$1
-shift
+DEPLOYMENT_BUCKET_NAME=$2
+
+shift 2
+
 PARAMS=""
 [ -f /app/params.yaml ] && PARAMS="-d=yaml:params.yaml"
 
-JSON=$(aws cloudformation describe-stacks --region ${AWS_REGION:-ap-southeast-2} --stack-name $STACKNAME)
+NOSTACK=""
+JSON=$(aws cloudformation describe-stacks --region ${AWS_REGION:-ap-southeast-2} --stack-name $STACKNAME) || $NOSTACK
+
+set -eo pipefail
+
 if [ -n "${JSON}" ]; then
   STATUS=$(echo ${JSON} | jq -r '.Stacks[0].StackStatus')
   if [ "${STATUS}" == "CREATE_FAILED" ]; then
@@ -16,4 +24,6 @@ if [ -n "${JSON}" ]; then
   fi
 fi
 
-rainbow --update-stack-if-exists -v -r ${AWS_REGION:-ap-southeast-2} ${PARAMS} ${STACKNAME} stack.json "$@"
+echo "--- deploy through rainbow python3 port"
+
+rainbow --update-stack-if-exists -v -r ${AWS_REGION:-ap-southeast-2} --deployment-bucket-name ${DEPLOYMENT_BUCKET_NAME} ${PARAMS} ${STACKNAME} stack.json "$@"
